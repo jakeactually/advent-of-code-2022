ac8d9faadbcf37ed4f39448a27134113bf5659f5
Jeyko Caicedo <jakeactually@outlook.com>
2023-01-22 14:35:53 -0500

    day 4

---
+++ 04/a.hs
@@ -0,0 +1,34 @@
+import Text.Parsec ( char, digit, many, parse, Parsec )
+
+type Range = (Int, Int)
+
+main :: IO ()
+main = do
+    text <- readFile "input.txt"
+    let info = map parseRanges $ lines text
+    let contained = flip filter info $ \(a, b) -> a `contains` b || b `contains` a
+    print $ length contained
+
+contains :: Range -> Range -> Bool
+contains (a1, b1) (a2, b2) = a1 <= a2 && b1 >= b2
+
+parseRanges :: String -> (Range, Range)
+parseRanges line = rs
+    where
+        Right rs = parse ranges "" line
+
+number :: Parsec String () Int
+number = read <$> many digit
+
+tuple :: Parsec String () a -> Char -> Parsec String () (a, a)
+tuple f sep = do
+    a <- f
+    char sep
+    b <- f
+    return (a, b)
+
+range :: Parsec String () Range
+range = tuple number '-'
+
+ranges :: Parsec String () (Range, Range)
+ranges = tuple range ','
