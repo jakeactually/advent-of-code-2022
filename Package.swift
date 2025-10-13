a6a3d4a6bce754791317f9b0b9f5d4174780b73e
Jeyko Caicedo <jakeactually@outlook.com>
2025-10-13 23:24:30 +0000

    delete swift files

--- Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
-    name: "swift-empty"
-)
