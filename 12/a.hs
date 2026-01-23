58a7512228a6ba1cf69666436c20ef8aca6717a1
Jeyko Caicedo <jakeactually@outlook.com>
2026-01-23 19:31:04 +0000

    day 12 a / grok

--- 12/a.hs
+++ 12/a.hs
@@ -1,58 +1,73 @@
 import qualified Data.Map as Map
 import Data.Map (Map)
 import qualified Data.Set as Set
-import Data.Set (Set)
+import Data.Set (Set, minView)
 import Data.Char (ord)
 import Data.List (find)
 import Data.Maybe (isJust)
 import Control.Monad (guard)
+import Data.Foldable (foldl')
+import Debug.Trace (trace)
 
-type Walker = ((Int, Int), Set (Int, Int), [(Int, Int)])
+type Pos = (Int, Int)
 
-parseInput :: String -> Map (Int, Int) Char
+newtype PrioPos = PrioPos (Int, Pos) -- steps, pos
+
+instance Ord PrioPos where
+  compare (PrioPos (s1, p1)) (PrioPos (s2, p2)) = compare s1 s2 `mappend` compare p1 p2
+
+instance Eq PrioPos where
+  PrioPos (s1, p1) == PrioPos (s2, p2) = s1 == s2 && p1 == p2
+
+parseInput :: String -> Map Pos Char
 parseInput input = Map.fromList $ do
-    (row, line) <- zip [0..] (lines input)
-    (col, char) <- zip [0..] line
-    return ((row, col), char)
+  (row, line) <- zip [0..] (lines input)
+  (col, char) <- zip [0..] line
+  return ((row, col), char)
 
 getHeight :: Char -> Int
 getHeight 'S' = ord 'a'
 getHeight 'E' = ord 'z'
 getHeight c = ord c
 
-neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors :: Pos -> [Pos]
 neighbors (r, c) = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
 
-generateNewWalkers :: Map (Int, Int) Char -> (Int, Int) -> Set (Int, Int) -> [(Int, Int)] -> [Walker]
-generateNewWalkers grid pos visited path = do
+getCandidates :: Map Pos Char -> Pos -> Int -> Map Pos Int -> [Pos]
+getCandidates grid pos steps minSteps = do
     npos <- neighbors pos
     guard (isJust (Map.lookup npos grid) &&
-           getHeight (grid Map.! npos) <= getHeight (grid Map.! pos) + 1 &&
-           not (Set.member npos visited))
-    return (npos, Set.insert npos visited, npos : path)
+      getHeight (grid Map.! npos) <= getHeight (grid Map.! pos) + 1 &&
+      maybe True (newSteps <) (Map.lookup npos minSteps))
+    return npos
+  where newSteps = steps + 1
 
-bfs :: Map (Int, Int) Char -> (Int, Int) -> (Int, Int) -> Maybe [(Int, Int)]
-bfs grid start endPos = go [(start, Set.singleton start, [start])]
+updateQueue :: Map Pos Char -> Pos -> Int -> Map Pos Int -> Set PrioPos -> (Map Pos Int, Set PrioPos)
+updateQueue grid pos steps minSteps pq =
+    foldl' (\(ms, pq) npos -> (Map.insert npos newSteps ms, Set.insert (PrioPos (newSteps, npos)) pq))
+      (minSteps, pq) candidates
   where
-    go :: [Walker] -> Maybe [(Int, Int)]
-    go [] = Nothing
-    go ((pos, visited, path):queue)
-        | pos == endPos = Just (reverse path)
-        | otherwise = go (queue ++ generateNewWalkers grid pos visited path)
+    candidates = getCandidates grid pos steps minSteps
+    newSteps = steps + 1
 
-findPath :: Map (Int, Int) Char -> Maybe [(Int, Int)]
-findPath grid = bfs grid start endPos
+findPath :: Map Pos Char -> Maybe Int
+findPath grid = loop grid endPos (Set.singleton (PrioPos (0, start))) (Map.singleton start 0)
   where
     start = head [pos | (pos, 'S') <- Map.toList grid]
     endPos = head [pos | (pos, 'E') <- Map.toList grid]
 
+loop :: Map Pos Char -> Pos -> Set PrioPos -> Map Pos Int -> Maybe Int
+loop grid endPos pq minSteps = case minView pq of
+  Nothing -> Nothing
+  Just (PrioPos (steps, pos), rest) -> debug $ if pos == endPos then Just steps else loop grid endPos newPq newMinSteps
+    where
+      debug = trace ("Processing: pos " ++ show pos ++ " with " ++ show steps ++ " steps")
+      (newMinSteps, newPq) = updateQueue grid pos steps minSteps rest
+
 main :: IO ()
 main = do
-    input <- readFile "input.txt"
-    let grid = parseInput input
-    case findPath grid of
-        Just path -> do
-            putStrLn "Path found:"
-            print path
-            putStrLn ("Length: " ++ show (length path - 1))
-        Nothing -> putStrLn "No path found"
+  input <- readFile "input.txt"
+  let grid = parseInput input
+  case findPath grid of
+    Just steps -> putStrLn ("Steps: " ++ show steps)
+    Nothing -> putStrLn "No path found"
