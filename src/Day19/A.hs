7e93d24535543768a3088066c3ced18afe9b3755
Jeyko Caicedo <jakeactually@outlook.com>
2026-03-25 23:40:05 +0000

    day 19 a parse

--- src/Day19/A.hs
+++ src/Day19/A.hs
@@ -1,7 +1,27 @@
 module Day19.A (main) where
 
+import Text.Regex.PCRE
+import qualified Data.Map as M
+import Data.List.Split (chunksOf)
+
+data Element = Ore | Clay | Obsidian | Geode deriving (Show, Eq, Ord)
+
+type Blueprint = M.Map Element [(Int, Element)]
+
+parseBlueprint :: [Int] -> Blueprint
+parseBlueprint [_, ore_ore, clay_ore, obs_ore, obs_clay, geo_ore, geo_obs] =
+    M.fromList [
+        (Ore, [(ore_ore, Ore)]),
+        (Clay, [(clay_ore, Ore)]),
+        (Obsidian, [(obs_ore, Ore), (obs_clay, Clay)]),
+        (Geode, [(geo_ore, Ore), (geo_obs, Obsidian)])
+    ]
+parseBlueprint _ = error "Invalid blueprint format"
+
 main :: IO ()
 main = do
   content <- readFile "input.txt"
-  let _theLines = lines content
-  putStrLn "Day 19 Part A scaffold ready."
+  let ints = getAllTextMatches (content =~ "[-+]?\\d+") :: [String]
+      parsedInts = map read ints :: [Int]
+      blueprints = map parseBlueprint (chunksOf 7 parsedInts)
+  print blueprints
