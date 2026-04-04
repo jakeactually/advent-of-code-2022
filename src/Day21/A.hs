2db175f31f252aa4288e65c5f37cd46c7ba21a00
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-04 20:00:55 +0000

    day 21 a, parse, gemini

--- src/Day21/A.hs
+++ src/Day21/A.hs
@@ -1,6 +1,42 @@
 module Day21.A (main) where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data Expr
+  = Sum Expr Expr
+  | Sub Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Var String
+  | Val Int
+  deriving (Show, Eq)
+
+parseLine :: String -> (String, Expr)
+parseLine line =
+  case words line of
+    (nameStr : rest) ->
+      let name = init nameStr
+       in case rest of
+            [val] -> (name, Val (read val))
+            [left, op, right] ->
+              let leftExpr = Var left
+                  rightExpr = Var right
+                  expr = case op of
+                    "+" -> Sum leftExpr rightExpr
+                    "-" -> Sub leftExpr rightExpr
+                    "*" -> Mul leftExpr rightExpr
+                    "/" -> Div leftExpr rightExpr
+                    _ -> error $ "Unknown operator: " ++ op
+               in (name, expr)
+            _ -> error "Invalid line"
+    [] -> error "Empty line"
+
+parseInput :: String -> Map String Expr
+parseInput = Map.fromList . map parseLine . lines
+
 main :: IO ()
 main = do
-  _ <- readFile "input.txt"
-  print "Day 21 Part A Not Implemented"
+  content <- readFile "input.txt"
+  let parsedMap = parseInput content
+  print $ Map.lookup "root" parsedMap
