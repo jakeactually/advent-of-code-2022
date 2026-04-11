8771f82d874aa71f949a6cb042f6f3b7f62078af
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-11 20:02:31 +0000

    day 22 parse

--- src/Day22/A.hs
+++ src/Day22/A.hs
@@ -1,6 +1,17 @@
 module Day22.A (main) where
 
+import Data.List (unsnoc)
+import Data.Map (Map, fromList)
+
 main :: IO ()
 main = do
-  _ <- readFile "input.txt"
-  print "Day 22 Part A Not Implemented"
+  file <- readFile "input.txt"
+  let Just (chartLines, path) = unsnoc $ lines file
+  let chart = chartLinesToMap $ init chartLines
+  print chart
+
+chartLinesToMap :: [String] -> Map (Int, Int) Char
+chartLinesToMap chartLines = fromList $ do
+  (y, chartLine) <- zip [0 ..] chartLines
+  (x, char) <- zip [0 ..] chartLine
+  return ((x, y), char)
