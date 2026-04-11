2d1c01242ce8d8425eb22bcaa4e0e5234462750b
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-11 20:15:42 +0000

    day 22 parse path - gemini

--- src/Day22/A.hs
+++ src/Day22/A.hs
@@ -2,16 +2,30 @@ module Day22.A (main) where
 
 import Data.List (unsnoc)
 import Data.Map (Map, fromList)
+import Text.Parsec (char, digit, many1, parse, (<|>))
+import Text.Parsec.String (Parser)
+
+data Action = Forward Int | TurnLeft | TurnRight deriving (Show, Eq)
 
 main :: IO ()
 main = do
   file <- readFile "input.txt"
-  let Just (chartLines, path) = unsnoc $ lines file
+  let Just (chartLines, pathStr) = unsnoc $ lines file
   let chart = chartLinesToMap $ init chartLines
-  print chart
+  let Right path = parse parsePath "" pathStr
+  print path
+
+parseAction :: Parser Action
+parseAction =
+  (Forward . read <$> many1 digit)
+    <|> (TurnLeft <$ char 'L')
+    <|> (TurnRight <$ char 'R')
+
+parsePath :: Parser [Action]
+parsePath = many1 parseAction
 
 chartLinesToMap :: [String] -> Map (Int, Int) Char
 chartLinesToMap chartLines = fromList $ do
   (y, chartLine) <- zip [0 ..] chartLines
-  (x, char) <- zip [0 ..] chartLine
-  return ((x, y), char)
+  (x, chr) <- zip [0 ..] chartLine
+  return ((x, y), chr)
