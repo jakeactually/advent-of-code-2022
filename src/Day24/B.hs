1691cbbc7e366ee2a20dd9f0f77460c23e3d91db
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-13 21:53:58 +0000

    day 24 b - cursor

--- src/Day24/B.hs
+++ src/Day24/B.hs
@@ -67,19 +67,21 @@ neighbors (r, c) = [(r, c), (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
 isInsideInner :: (Int, Int) -> Pt -> Bool
 isInsideInner (height, width) (r, c) = r >= 1 && r <= height - 2 && c >= 1 && c <= width - 2
 
-shortestTime :: (Int, Int) -> Pt -> Pt -> Array Int (Set Pt) -> Int
-shortestTime dims@(height, width) start goal occupiedCache = bfs initialQueue initialSeen
+-- | Minimum minutes to reach @legGoal@ from @legStart@, when the expedition may
+-- begin moving at absolute minute @t0@ (blizzard state at minute @m@ uses @m \`mod\` cycleLen@).
+shortestTimeFrom :: (Int, Int) -> Pt -> Pt -> Int -> Array Int (Set Pt) -> Int
+shortestTimeFrom dims@(height, width) legStart legGoal t0 occupiedCache = bfs initialQueue initialSeen
   where
     cycleLen = lcm (height - 2) (width - 2)
-    initialQueue = Seq.singleton (start, 0)
-    initialSeen = Set.singleton (start, 0)
+    initialQueue = Seq.singleton (legStart, t0)
+    initialSeen = Set.singleton (legStart, t0 `mod` cycleLen)
 
-    isValid p = p == start || p == goal || isInsideInner dims p
+    isValid p = p == legStart || p == legGoal || isInsideInner dims p
 
     bfs :: Seq (Pt, Int) -> Set (Pt, Int) -> Int
     bfs Seq.Empty _ = error "no path found"
     bfs ((pos, t) :<| q) seen
-      | pos == goal = t
+      | pos == legGoal = t
       | otherwise = bfs nextQueue nextSeen
       where
         t' = t + 1
@@ -96,5 +98,9 @@ main :: IO ()
 main = do
   content <- readFile "input.txt"
   let (height, width, start, goal, blizzards) = parseInput content
-  let occupiedCache = buildOccupiedCache (height, width) blizzards
-  print (shortestTime (height, width) start goal occupiedCache)
+  let dims = (height, width)
+  let occupiedCache = buildOccupiedCache dims blizzards
+  let t1 = shortestTimeFrom dims start goal 0 occupiedCache
+  let t2 = shortestTimeFrom dims goal start t1 occupiedCache
+  let t3 = shortestTimeFrom dims start goal t2 occupiedCache
+  print t3
