d718900841daad795cbd5bbc23ee38e41c08a1ac
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-14 03:03:57 +0000

    day 25 a - no ai - decoding part

--- src/Day25/A.hs
+++ src/Day25/A.hs
@@ -3,5 +3,23 @@ module Day25.A (main) where
 main :: IO ()
 main = do
   content <- readFile "input.txt"
-  let _theLines = lines content
-  putStrLn "Day 25 Part A scaffold ready."
+  let theLines = lines content
+  -- mapM_ (print . wordToNumber) theLines
+  print $ sum $ map wordToNumber theLines
+
+charToNumber :: Char -> Int
+charToNumber chr = case chr of
+  '2' -> 2
+  '1' -> 1
+  '0' -> 0
+  '-' -> -1
+  '=' -> -2
+  _ -> error "Invalid character"
+
+powersOf5 :: [Int]
+powersOf5 = map (5 ^) [0..]
+
+wordToNumber :: String -> Int
+wordToNumber word = sum $ zipWith (*) coeffs powersOf5
+  where
+    coeffs = map charToNumber $ reverse word
